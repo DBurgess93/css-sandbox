8908bd8882fe7d87070ad684a42610ce02683e70
Daniel Burgess <dan-burgess@hotmail.com>
2023-06-16 14:16:20 +0930

    Playing around with css grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import './App.scss'
 
 function App() {
   return (
-    <>
-
-    </>
+    <div className="container">
+      <header>Header</header>
+      <main>Content</main>
+      <nav>Navigation</nav>
+      <aside>Sidebar</aside>
+      <footer>Footer</footer>
+    </div>
   );
 }
 
